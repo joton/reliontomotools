4a98de14f43a2e09b876aa79310a2a3cbd55b353
Joaquin Oton <joaquin.oton@gmail.com>
2021-06-14 15:30:25 +0100

    Added dependencies to setup.py and other metadata

--- setup.py
+++ setup.py
@@ -1,16 +1,36 @@
 from setuptools import setup
 
-pkg = 'reliontomotools'
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 
+pkg = 'reliontomotools'
 exec(open(f'{pkg}/_version.py').read())
 
 setup(
       name=pkg,
       version=__version__,
+      author="Joaquín Otón",
+      description="External tools for Relion tomo",
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url="https://github.com/joton/reliontomotools",
+      project_urls={
+        "Bug Tracker": "https://github.com/joton/reliontomotools/issues",
+                    },
+      classifiers=[
+          "Programming Language :: Python :: 3",
+          "License :: OSI Approved :: MIT License",
+          "Operating System :: OS Independent",
+                  ],
       packages=[pkg],
+      license='MIT',
       entry_points={
           "console_scripts": [
               f"warptomo2relion = {pkg}.warptomo2relion:warpTomo2RelionProgram"
-              ]
-          },
-      )
+                             ]
+                   },
+      python_requires=">=3.6",
+      install_requires=['numpy', 'pandas', 'xmltodict', 'docopt',
+                        'scipy', 'mrcfile', 'tqdm']
+
+     )
